6995c3efb3a59b262db2cb0a06b2cdfe0ad4fecd
dangdtr <ddang.daodang@gmail.com>
2023-02-08 01:39:10 +0700

    Make change 2

--- src/main/java/com/uet/app/A.java
+++ src/main/java/com/uet/app/A.java
@@ -2,8 +2,6 @@ package com.uet.app;
 
 public class A {
     public String methodA() {
-       int i = 0;
-       i+=1;
         return "method of Class A";
     }
 }
--- src/main/java/com/uet/app/B.java
+++ src/main/java/com/uet/app/B.java
@@ -2,6 +2,8 @@ package com.uet.app;
 
 public class B extends A {
     public String methodB() {
+        int i = 0;
+        i+=2;
         return "method of Class B";
     }
 }
